883d4a415aea9a5526e84c0f6ce9ec301fc33b8c
lisadelladucata <lisa.delladucata@gmail.com>
2025-05-19 17:16:21 +0200

    feat:add-exercise

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Movie;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        $movies = Movie::all();
+        return view('home', compact('movies'));
+    }
+}
